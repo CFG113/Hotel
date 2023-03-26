9bda04d96640a068001b50cd16c99cdf56eb6e5c
nikitapestov <instagramw7@gmail.com>
2023-03-26 18:31:43 +1100

    Nigga

--- hotel.java
+++ hotel.java
@@ -2,5 +2,7 @@ public class hotel {
 
   public static void main(String[] args) throws Exception {
     System.out.println("Hello, World!");
+    System.out.println("Hello from NUPUP!");
+    
   }
 }
